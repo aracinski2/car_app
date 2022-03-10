1d5bc87b3f85b25aac2c8ca1e4ab3f6817506db7
Alex Racinski <aracinski2@gmail.com>
2022-03-09 18:56:21 -0600

    add index action, add hardcoded show action

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -1,6 +1,11 @@
 class Api::CarsController < ApplicationController
   def index
-    @cars = "vrooom"
+    @cars = Car.all
     render "index.json.jb"
+  end
+
+  def show 
+    @car = Car.find_by(id:1)
+    # @car = Car.find_by(id:[params:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get "/cars" => "cars#index"
+    get "/cars/:id" => "cars#show"
   end
 
 end
